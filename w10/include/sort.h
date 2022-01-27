33e6690cf0ca26992947796d1dc04eba56ca7727
hoangthaihau <75879525+hoangthaihau@users.noreply.github.com>
2022-01-27 12:06:43 +0700

    Add files via upload

---
+++ w10/include/sort.h
@@ -0,0 +1,14 @@
+void insertionSort(int a[], int size);
+void selectionSort(int a[], int size);
+
+// merge sort
+void merge(int arr[], int l, int m, int r);
+void mergeSort(int a[], int left, int right);
+
+//quick sort
+int partition(int a[], int left, int right);
+void quickSort(int a[], int left, int right);
+
+//heap sort
+void adjust(int a[], int n, int i);
+void heapSort(int a[], int n);
